e7a749189d01d6bff55617ffb16d327d3b9d8e84
Izaias Lima <izaiaslima356@gmail.com>
2024-03-11 12:14:55 -0300

    fix: cypress build error

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,19 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const env = createEnv({
-	server: {
-		APP_URL: z.string().url(),
-	},
-	client: {
-		NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-	},
-	runtimeEnv: {
-		APP_URL: process.env.DATABASE_URL,
-		NEXT_PUBLIC_API_BASE_URL: process.env.OPEN_AI_API_KEY,
-	},
+const envSchema = z.object({
+	NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+	APP_URL: z.string().url(),
 });
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+	console.error(
+		"Invalid environment variables",
+		parsedEnv.error.flatten().fieldErrors
+	);
+
+	throw new Error("Invalid environment variables");
+}
+
+export const env = parsedEnv.data;
